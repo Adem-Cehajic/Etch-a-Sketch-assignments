421a875567ef3796ef9fed3b502b4382f749057a
Adem Cehajic <ademc1191@gmail.com>
2024-11-15 15:20:16 -0500

    Added drawing mechanics
    I added a button that would clear the board and a way to change the board size as well as implementing drawing functionality in js by having the color change when the user hovers over it

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-let r = document.querySelector('.canvas')
-for (let i = 0; i < 5; i++) {
-    let k = document.createElement('div');
+let r = document.querySelector('.canvas');
+let inp = 16;
+function makecanvas(size) {
+    while (r.hasChildNodes()) {
+        r.removeChild(r.firstChild);
+    }
+    for (let i = 0; i < size; i++) {
+        let k = document.createElement('div');
+        for(let i = 0; i < size; i++) {
+            let v = document.createElement('div');
+            k.appendChild(v);
+    }
     r.appendChild(k);
+    }
+    g = document.querySelectorAll('.canvas > div > div');
+    g.forEach( (div) => {
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = "black";
+        })
+        
+    });
 }
+makecanvas(inp);
+const but = document.querySelector('button');
+but.addEventListener('click', () => {
+    inp = parseInt(window.prompt('Select canvas size'));
+    makecanvas(inp);
+})
+const res = document.querySelectorAll('button')[1];
+res.addEventListener('click', () => {
+    makecanvas(inp);
+})
